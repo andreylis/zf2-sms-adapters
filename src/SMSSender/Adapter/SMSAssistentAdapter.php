db6f09a60dc97b0f9aa3a891d2fcc2d795c322aa
Andrey Lis <me@andreylis.ru>
2013-08-05 19:19:51 +0300

    And some more refactoring

--- src/SMSSender/Adapter/SMSAssistentAdapter.php
+++ src/SMSSender/Adapter/SMSAssistentAdapter.php
@@ -8,35 +8,36 @@ namespace SMSSender\Adapter;
 use SMSSender\Adapter\AdapterInterface;
 use SMSSender\Entity\Message;
 use SMSSender\Exception\RuntimeException;
-use SMSSender\Module as Module;
+use SMSSender\Service\OptionsTrait;
 use Zend\Http\Client;
-use Zend\Http\Client\Adapter\Curl;
+use Zend\ServiceManager\ServiceLocatorAwareInterface;
+use Zend\ServiceManager\ServiceLocatorAwareTrait;
 
-class SMSAssistentAdapter implements AdapterInterface
+class SMSAssistentAdapter implements AdapterInterface, ServiceLocatorAwareInterface
 {
+
+    use ServiceLocatorAwareTrait, OptionsTrait;
 
     /**
      * @param Message $message
+     * @throws RuntimeException
      * @return Message
      */
     public function send(Message $message)
     {
-        $moduleObject=  new Module();
-        $config = $moduleObject->getConfig();
-        $smsAssistentConfig = $config['sms'];
+        $config = $this->getSenderOptions();
 
         $serviceURL = "https://sys.sms-assistent.ru/api/v1/send_sms/plain?";
 
         $queryURL = $serviceURL . http_build_query([
-                'user' => $smsAssistentConfig['username'],
-                'password' => $smsAssistentConfig['password'],
+                'user' => $config->getUsername(),
+                'password' => $config->getPassword(),
+                'sender' => $config->getSender(),
                 'recipient' => str_replace(["+", " ", '-'], "", $message->getRecipient()), // на всякий случай
                 'message' => $message->getMessage(),
-                'sender' => $smsAssistentConfig['sender'],
             ]);
 
         $client =  new Client();
-        $client->setAdapter((new Curl()));
         $client->setUri($queryURL);
 
         try {
